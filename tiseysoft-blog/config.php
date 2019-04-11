9d5617f1c5af3f42b69922ab933c700cb0761613
tiseysoft <tiseysoft@yahoo.com>
2019-04-10 22:26:01 -0700

    all my files here

---
+++ tiseysoft-blog/config.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+//connect to db here 
+$conn = mysqli_connect("localhost", "root", "", "tiseysoft-blog");
+if(!$conn){
+    die("failed to connect" . mysqli_connect_error());
+}
+//coming soon... 
+define ('ROOT_PATH', realpath(dirname(__FILE__)));
+define ('BASE_URL', 'http://http://localhost/tiseysoft-blog/');
+?>
